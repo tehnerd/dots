07ee83c72318f801756317c27df0376f4b2d299a
tehnerd <tehnerd@tehnerd.com>
2017-11-12 13:04:38 -0800

    hint for nuclide how to

---
+++ install_nvm_nuclide.sh
@@ -0,0 +1,6 @@
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
+bash install_nvm.sh
+bash
+#nvm ls-remote
+#nvm install ..
+#npm install -g nuclide@<ver>
