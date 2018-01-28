fcdc8bb62d89520d26dc1bffb907fc4d6f6eb02c
tehnerd <tehnerd@tehnerd.com>
2018-01-27 21:36:34 -0800

    addign A, removing YCM

--- install.sh
+++ install.sh
@@ -5,17 +5,9 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/.vim/bundle
-git clone https://github.com/scrooloose/syntastic.git
+git clone --depth 1 https://github.com/scrooloose/syntastic.git
+git clone --depth 1 https://github.com/scrooloose/nerdtree.git
+git clone --depth 1 https://github.com/majutsushi/tagbar
+git clone --depth 1 https://github.com/vim-scripts/a.vim
 
-cd ~/.vim/bundle
-git clone https://github.com/fatih/vim-go.git
 
-cd ~/.vim/bundle
-git clone https://github.com/scrooloose/nerdtree.git
-
-git clone https://github.com/majutsushi/tagbar
-git clone https://github.com/Valloric/YouCompleteMe
-cd YouCompleteMe
-git submodule update --init --recursive
-sudo apt-get install cmake ctags python-dev g++
-./install.py --clang-completer
